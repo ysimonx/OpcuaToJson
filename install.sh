4f528c11d782daab8eed0c4c8133c5744ac2a387
Yannick Simon <yannick.simon@gmail.com>
2023-03-12 21:04:09 +0100

    upd

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt install python3-venv
 python3 -m venv ./env
 source ./env/bin/activate
-pip install opcua, jsonpath_ng, cryptography
+pip install opcua jsonpath_ng cryptography
 # pip install -r requirements.txt
